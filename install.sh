224e521233cf86abbe3f1336be7616d4a6244cdd
matteo <matteo.boursault.pro@gmail.com>
2026-09-05 10:59:07 +0200

    :wrench: ajout script d'installation

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo pacman -Syu \
+  podman \
+  distrobox \
+  kitty \
+  neovim \
+  keepassxc \
+  git \
+  spotify-launcher \
+  steam \
+  cachyos-niri-noctalia \
+  sddm \
+
+sudo systemctl start NetworkManager ssdm
